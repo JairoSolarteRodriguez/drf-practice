d4b4733ff1181821f90634f5c395c7cf106f48f4
JairoSolarteRodriguez <pvprandon578@gmail.com>
2022-04-25 14:27:00 -0500

    create models for user

--- profiles_project/profiles_project/settings.py
+++ profiles_project/profiles_project/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
+    #DRF
+    'rest_framework',
+    'rest_framework.authtoken', 
+
+    #my apps
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
