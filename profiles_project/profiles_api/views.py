aafe10ab3db5766a51d0c3b7ed8249a9a3e5a074
JairoSolarteRodriguez <pvprandom578@gmail.com>
2022-04-25 19:51:12 -0500

    first view in django rest framework

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
+from rest_framework.views import APIView, Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """ API view test """
+
+    def get(self, request, format=None):
+        """ return list of api view characteristics"""
+        an_apiview = [
+            "usamos metodos htpp como funciones (get, post, patch, delete, put)",
+            "Es similar a una vista tradicional de django",
+            "nos da mayor control sobre la logica de la aplicacione",
+            "Mapeado manualmente a los URLs",
+        ]
+
+        # convierte la info a json (tiene que ser una lista o un diccionario)
+        return Response({
+            "message" : "Hello",
+            "an_apiview": an_apiview
+        })
